e34d829d55d15959eae954adf2731571f803de14
kkrystof <krystofus.kulhanek@gmail.com>
2022-10-15 17:30:01 +0200

    ready api endpoint - test mode

--- pages/api/room.ts
+++ pages/api/room.ts
@@ -4,6 +4,10 @@ import { AccessToken } from 'livekit-server-sdk';
 import crypto from 'crypto'
 import randomstring from 'randomstring'
 
+import Database from 'better-sqlite3';
+const db = new Database('./pages/api/db/rooms.db');
+
+
 
 // pages/api/user
 export async function getData(userName: string, roomId: string) {
@@ -15,29 +19,49 @@ export async function getData(userName: string, roomId: string) {
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { userName, roomId } = req.body
+  const roomExist = db.prepare('select 1 from rooms where roomId = ?');
+  const addRoom = db.prepare('insert into rooms (roomId, createdBy) VALUES (?, ?)');
+
+  const { userName, roomId } = req.body
+
+  // let roomName
+
+  if(!userName)
+    res.status(400).json({error: 'not userName'})
+    
+  if(roomId && !roomExist.get(roomId))
+    res.status(400).json({error: 'did not find this room'})
+    
+    
+
+    const roomName = (roomExist.get(roomId)) ? roomId : genRoomId()
+    addRoom.run(roomName, userName)
 
 
-    const roomName = (roomId) ? roomId : genRoomId()
 
 
-    const at = new AccessToken(process.env.LK_API_KEY, process.env.LK_API_SECRET, {identity: userName});
+    // const at = new AccessToken(process.env.LK_API_KEY, process.env.LK_API_SECRET, {identity: userName});
 
-    at.addGrant({ roomJoin: true, room: roomName });
-    const token = at.toJwt();
+    // at.addGrant({ roomJoin: true, room: roomName });
+    // const token = at.toJwt();
 
 
-    if(token)
+
+    const token = 'test-TOKEN'
+
+
+    // if(token)
         res.status(200).json({roomId: roomName, token: token})
 
   }
 
+// room id generation
   const genRoomId = () => {
-
+    
     const id = randomstring.generate({
       length: 9,
       charset: 'abcdefghijklmnopqrstuvwxyz'
     }).match(/.{1,3}/g)
 
-    return `${id[0]}-${id[1]}-${id[2]}`;
+    return `${id[0]}-${id[1]}-${id[2]}`
   }
