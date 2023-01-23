a6d9f9ff00daac672f2005026bb085e209739ae6
kkrystof <krystofus.kulhanek@gmail.com>
2023-01-23 02:00:14 +0100

    type updated

--- styled.d.ts
+++ styled.d.ts
@@ -14,6 +14,8 @@ declare module 'styled-components' {
         '200' : string
         '300' : string
         '400' : string
+        '500' : string
+        '600' : string
       }
       primary: string
       secondary: string
